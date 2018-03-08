1974d5216cbffb873415670638d85ebfcd4f47f6
Åsa Liljegren <asa.lilje@gmail.com>
2018-03-08 23:18:14 +0100

    Adding code

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloFromCore.Controllers
+{
+    [Route("hello")]
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Get()
+        {
+            return Ok("Hello world from Core on a Raspi!");
+        }
+
+    }
+
+}
